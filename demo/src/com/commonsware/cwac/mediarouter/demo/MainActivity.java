76ed7216da7ffd0d7b957fb9d000995359cf272a
Mark Murphy <mmurphy@commonsware.com>
2015-03-04 11:02:15 -0500

    switched demo lifecycle methods per issue #4

--- demo/src/com/commonsware/cwac/mediarouter/demo/MainActivity.java
+++ demo/src/com/commonsware/cwac/mediarouter/demo/MainActivity.java
@@ -45,18 +45,18 @@ public class MainActivity extends Activity {
   }
 
   @Override
-  public void onResume() {
-    super.onResume();
+  public void onStart() {
+    super.onStart();
 
     router.addCallback(selector, cb,
                        MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
   }
 
   @Override
-  public void onPause() {
+  public void onStop() {
     router.removeCallback(cb);
 
-    super.onPause();
+    super.onStop();
   }
 
   @Override
